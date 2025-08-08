b68629078e174e7f9b5bab01d5808c7b9b657214
Tristan NGUYEN <trungupdate@gmail.com>
2025-08-08 23:18:31 +0700

    Add ERC1155Token contract for minting and batch minting of tokens

---
+++ backend-hedera-contracts/AMOCA-NFT-1155.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+// Compatible with OpenZeppelin Contracts ^5.0.0
+pragma solidity ^0.8.22;
+
+import {ERC1155} from "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+import {ERC1155Burnable} from "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155Burnable.sol";
+
+/// @custom:security-contact amocahealth@gmail.com
+
+contract ERC1155Token is ERC1155, Ownable, ERC1155Burnable {
+    constructor(address initialOwner)
+        ERC1155("")
+        Ownable(initialOwner)
+    {}
+
+    function mint(address account, uint256 id, uint256 amount, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mint(account, id, amount, data);
+    }
+        
+    function mintBatch(address to, uint256[] memory ids, uint256[] memory amounts, bytes memory data)
+        public
+        onlyOwner
+    {
+        _mintBatch(to, ids, amounts, data);
+    }
+        
+}
